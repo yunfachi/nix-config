a82e4019ecb412f780359f2d1c819daf61096e34
yunfachi <yunfachi@gmail.com>
2023-10-23 17:46:47 +0300

    added ssh key "meido"

--- modules/user.nix
+++ modules/user.nix
@@ -19,6 +19,8 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINin10cad4eDG5u2W4ozQyZTINbjRLLnrFKo/WihL45r yunfachi@mitama"
       # servers
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIELCGMGAQ6cGOLs1wsKFRYg5ECtvvAAmY1jasJdlRtHT yunfachi@mitama"
+      # secrets
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCrv1ypoRRd4O4BohV40yr2g55dHI2/+y3VXX/EH28k yunfachi@mitama"
     ];
   };
 
