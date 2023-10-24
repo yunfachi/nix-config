a140669485afebdb703cad9f1b2101e2acdfefe0
yunfachi <yunfachi@gmail.com>
2023-10-24 17:02:04 +0300

    fixed padding and TERM in kitty

--- home/desktop/tools/kitty.nix
+++ home/desktop/tools/kitty.nix
@@ -3,10 +3,12 @@
     enable = true;
 
     settings = {
+      term = "xterm-256color";
       background_opacity = "0.25";
       confirm_os_window_close = 0;
       enable_audio_bell = false;
       scrollback_lines = 10000;
+      placement_strategy = "top-left";
     };
   };
 }
