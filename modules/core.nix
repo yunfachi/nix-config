52896fce8b938306fa629d278c3096f2803464cf
yunfachi <yunfachi@gmail.com>
2023-11-08 09:50:46 +0300

    feat: add time zone and locale

--- modules/core.nix
+++ modules/core.nix
@@ -12,6 +12,25 @@
     tmp.cleanOnBoot = true;
   };
 
+  time.timeZone = "Europe/Madagascar";
+  i18n = let
+    locale = "en_US.UTF-8";
+    extraLocale = "ja_JP.UTF-8";
+  in {
+    defaultLocale = locale;
+    extraLocaleSettings = {
+      LC_ADDRESS = extraLocale;
+      LC_IDENTIFICATION = extraLocale;
+      LC_MEASUREMENT = extraLocale;
+      LC_MONETARY = extraLocale;
+      LC_NAME = extraLocale;
+      LC_NUMERIC = extraLocale;
+      LC_PAPER = extraLocale;
+      LC_TELEPHONE = extraLocale;
+      LC_TIME = extraLocale;
+    };
+  };
+
   documentation = {
     enable = lib.mkDefault false;
     doc.enable = lib.mkDefault false;
