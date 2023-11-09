5fe1aeb07fbffd9985c88e34acc42ee105314a8d
yunfachi <yunfachi@gmail.com>
2023-11-09 17:35:08 +0300

    fix: change pm2 service style for home manager

--- home/server/pm2.nix
+++ home/server/pm2.nix
@@ -3,14 +3,11 @@
     nodePackages_latest.pm2
   ];
 
-  systemd.services.pm2 = {
-    enable = true;
-    description = "pm2";
-    unitConfig = {
-      Type = "simple";
-    };
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
+  systemd.user.services."pm2" = {
+    Unit.Description = "pm2";
+    Unit.Type = "simple";
+    Install.WantedBy = ["multi-user.target"];
+    Service = {
       ExecStart = "${pkgs.nodePackages_latest.pm2}/bin/pm2 resurrect";
       ExecReload = "${pkgs.nodePackages_latest.pm2}/bin/pm2 reload all";
       ExecStop = "${pkgs.nodePackages_latest.pm2}/bin/pm2 kill";
