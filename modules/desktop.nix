72c7586bb73337aa9709b1647dd346f5dc4be7d6
yunfachi <yunfachi@gmail.com>
2023-11-22 12:12:28 +0000

    feat(desktop): add sounds

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -12,4 +12,18 @@
     enable = true;
     packages = [pkgs.dconf];
   };
+
+  # Sounds
+  services.pipewire = {
+    enable = true;
+    package = pkgs.pipewire;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+  security.rtkit.enable = true;
+  sound.enable = false;
+  hardware.pulseaudio.enable = false;
 }
