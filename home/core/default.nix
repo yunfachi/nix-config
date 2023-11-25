3782083b0fdcad2bdb84c048b06e2943ce9f9c67
Yun Fachi <yunfachi@gmail.com>
2023-11-25 10:22:06 +0000

    refactor(home): moved core.nix to the core folder

--- home/core/default.nix
+++ home/core/default.nix
@@ -1,6 +1,20 @@
-{...}: {
+{
+  lib,
+  username,
+  nixosVersion,
+  ...
+}: {
   imports = [
     ./git.nix
     ./tools.nix
   ];
+
+  home = {
+    username = username;
+    homeDirectory = lib.mkForce "/home/${username}";
+  };
+
+  programs.home-manager.enable = true;
+
+  home.stateVersion = nixosVersion;
 }
