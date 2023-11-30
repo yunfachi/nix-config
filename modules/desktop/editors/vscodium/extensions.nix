a0e2a6d8c1f6f8807469242300111ae83d4c77f1
yunfachi <yunfachi@gmail.com>
2023-11-30 18:32:25 +0300

    feat(desktop): add vscodium

---
+++ modules/desktop/editors/vscodium/extensions.nix
@@ -0,0 +1,28 @@
+{
+  inputs,
+  system,
+  ...
+}: {
+  programs.vscode = {
+    extensions = with inputs.vscode-extensions.extensions.${system}.vscode-marketplace; [
+      beardedbear.beardedtheme # Theme
+      beardedbear.beardedicons # Icons
+
+      wakatime.vscode-wakatime # WakaTime
+      leonardssh.vscord # Discord RPC
+
+      kamadorueda.alejandra # Nix Formatter
+      jnoortheen.nix-ide # Nix Syntax Highlight and more
+
+      github.vscode-github-actions # Github Actions
+
+      github.remotehub # Github remote repositories
+      ms-vscode-remote.remote-ssh # SSH
+
+      github.copilot # Copilot AI
+      github.copilot-chat # Copilot AI chat
+      github.copilot-labs # Copilot AI labs
+      github.heygithub # Copilot AI voice
+    ];
+  };
+}
