7791b230078fefb3a8935c48d0fdbac9e63ed278
yunfachi <yunfachi@gmail.com>
2023-11-30 19:07:02 +0300

    refactor: move programs from modules to home

--- home/desktop/xmonad/default.nix
+++ home/desktop/xmonad/default.nix
@@ -1,5 +1,16 @@
 {...}: {
-  xsession.windowManager.xmonad = {
-    config = ./xmonad.hs;
+  xsession = {
+    enable = true;
+
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+
+      extraPackages = hp: [
+        hp.dbus
+        hp.monad-logger
+      ];
+      config = ./xmonad.hs;
+    };
   };
 }
