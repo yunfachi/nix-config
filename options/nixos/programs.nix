8603cb75ffccf2d6ed99edf806f6ec81596d18d3
yunfachi <yunfachi@gmail.com>
2023-12-23 16:05:00 +0000

    feat(nixos): make grub optional

--- options/nixos/programs.nix
+++ options/nixos/programs.nix
@@ -3,11 +3,11 @@
   lib,
   type,
   ...
-}: let
+}:
+with lib; let
   cfg = config.yunfachi-nixos;
 in {
   options.yunfachi-nixos = {
-    archisteamfarm.enable = lib.mkEnableOption "archisteamfarm" // {default = false;};
-    hyprland.enable = lib.mkEnableOption "hyprland (nixos)" // {default = type == "desktop";};
+    hyprland.enable = mkEnableOption "hyprland" // {default = type == "desktop";};
   };
 }
