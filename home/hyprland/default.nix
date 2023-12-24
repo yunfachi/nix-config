382710ac16b77f1c8cf7912f722b444e153699c7
yunfachi <yunfachi@gmail.com>
2023-12-24 16:11:51 +0000

    style: remove unnecessary parentheses around primitive expressions and add inherits

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -25,12 +25,14 @@ in {
 
       settings = lib.mkMerge [
         {
-          monitor = map (
-            m: let
-              resolution = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
-              position = "${toString m.x}x${toString m.y}";
-            in "${m.name},${resolution},${position},${toString m.scale}"
-          ) (config.yunfachi.monitors);
+          monitor =
+            map (
+              m: let
+                resolution = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
+                position = "${toString m.x}x${toString m.y}";
+              in "${m.name},${resolution},${position},${toString m.scale}"
+            )
+            config.yunfachi.monitors;
 
           input = {
             kb_layout = "${
