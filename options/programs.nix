7d0c2ef21f85a128dc1f8c19e97f84055f55e441
yunfachi <yunfachi@gmail.com>
2024-01-08 10:45:59 +0000

    feat(home): add lunarclient

--- options/programs.nix
+++ options/programs.nix
@@ -11,6 +11,7 @@ in {
     firefox.enable = lib.mkEnableOption "firefox" // {default = type == "desktop";};
     git.enable = lib.mkEnableOption "git" // {default = true;};
     steam.enable = lib.mkEnableOption "steam" // {default = type == "desktop";};
+    lunarclient.enable = lib.mkEnableOption "lunarclient" // {default = type == "desktop";};
     hyprland.enable = lib.mkEnableOption "hyprland" // {default = type == "desktop";};
     kitty.enable = lib.mkEnableOption "kitty" // {default = type == "desktop";};
     shikimori.enable = lib.mkEnableOption "shikimori" // {default = type == "desktop";};
