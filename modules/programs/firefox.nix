859cfa8a986b95fc4828643ae810a70cd9772e03
yunfachi <yunfachi@gmail.com>
2024-02-03 14:35:34 +0000

    feat(firefox): add settings, extensions and bookmarks

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -2,6 +2,7 @@
   module-functions,
   hm,
   pkgs,
+  ypkgs,
   username,
   ...
 }:
@@ -11,8 +12,69 @@ module-functions.module "programs" "firefox" {
     package = pkgs.firefox-devedition;
 
     profiles."${username}" = {
-      extensions = [
+      bookmarks = [
+        {
+          toolbar = true;
+          bookmarks = [
+            {
+              name = "yunfachi";
+              url = "https://mail.google.com/mail/u/0/#inbox";
+            }
+            {
+              name = "dg";
+              url = "https://mail.google.com/mail/u/1/#inbox";
+            }
+            {
+              name = "search";
+              url = "https://search.nixos.org/options";
+            }
+            {
+              name = "watch";
+              url = "https://shikimori.one/yunfachi/list/anime?mylist=watching&order=rate_updated";
+            }
+            {
+              name = "translate";
+              url = "https://translate.yandex.ru/?source_lang=en&target_lang=ru";
+            }
+          ];
+        }
       ];
+
+      extensions = with ypkgs.firefox-addons; [
+        shikiplayer
+        coffee_theme_firefox
+      ];
+
+      settings = {
+        "browser.newtabpage.activity-stream.feeds.topsites" = false;
+        "browser.shell.checkDefaultBrowser" = false;
+        "browser.theme.toolbar-theme" = 0;
+        "browser.uiCustomization.state" = builtins.toJSON {
+          currentVersion = 20;
+          dirtyAreaCache = ["nav-bar" "PersonalToolbar" "toolbar-menubar" "TabsToolbar"];
+          newElementCount = 12;
+          placements = {
+            PersonalToolbar = ["personal-bookmarks"];
+            TabsToolbar = ["tabbrowser-tabs" "alltabs-button"];
+            nav-bar = ["back-button" "forward-button" "stop-reload-button" "urlbar-container" "downloads-button" "developer-button" "unified-extensions-button"];
+            toolbar-menubar = ["menubar-items"];
+            unified-extensions-area = [];
+            widget-overflow-fixed-list = [];
+          };
+          seen = ["save-to-pocket-button" "developer-button" "profiler-button"];
+        };
+        "browser.urlbar.shortcuts.bookmarks" = false;
+        "browser.urlbar.shortcuts.history" = false;
+        "browser.urlbar.shortcuts.quickactions" = false;
+        "browser.urlbar.shortcuts.tabs" = false;
+        "browser.urlbar.suggest.engines" = false;
+        "browser.urlbar.suggest.history" = false;
+        "browser.urlbar.suggest.openpage" = false;
+        "browser.urlbar.suggest.topsite" = false;
+        "extensions.activeThemeID" = "coffee_theme_firefox";
+        "extensions.pendingOperations" = true;
+        "privacy.trackingprotection.enabled" = true;
+      };
     };
   };
 }
