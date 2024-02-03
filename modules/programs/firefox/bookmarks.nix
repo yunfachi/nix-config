2d19d24a21b8fb93daa7be98386fdd45be4bae0d
yunfachi <yunfachi@gmail.com>
2024-02-03 14:48:40 +0000

    refactor(firefox): modularize the firefox module

---
+++ modules/programs/firefox/bookmarks.nix
@@ -0,0 +1,37 @@
+{
+  module-functions,
+  hm,
+  pkgs,
+  ypkgs,
+  username,
+  ...
+}:
+module-functions.module "programs" "firefox" {
+  hm.programs.firefox.profiles."${username}".bookmarks = [
+    {
+      toolbar = true;
+      bookmarks = [
+        {
+          name = "yunfachi";
+          url = "https://mail.google.com/mail/u/0/#inbox";
+        }
+        {
+          name = "dg";
+          url = "https://mail.google.com/mail/u/1/#inbox";
+        }
+        {
+          name = "search";
+          url = "https://search.nixos.org/options";
+        }
+        {
+          name = "watch";
+          url = "https://shikimori.one/yunfachi/list/anime?mylist=watching&order=rate_updated";
+        }
+        {
+          name = "translate";
+          url = "https://translate.yandex.ru/?source_lang=en&target_lang=ru";
+        }
+      ];
+    }
+  ];
+}
