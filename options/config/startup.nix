3c9d6d4d52f7df03b9fb0ed6fb363f6a96248b07
yunfachi <yunfachi@gmail.com>
2024-02-10 17:57:17 +0000

    refactor(config): rename the startup module to commands, add the onStartup and onReload options to hyprland.commands

--- options/config/startup.nix
+++
@@ -1,7 +0,0 @@
-{option-functions, ...}:
-with option-functions;
-  option null "startup" {
-    commands = {
-      onGraphical = listOption "shell scripts executed after loading the desktop" [] types.str;
-    };
-  }
