4682503abb222c8155edcccc7dc10032f21c4636
yunfachi <yunfachi@gmail.com>
2024-02-10 18:11:30 +0000

    feat: add border_size, rounding options to rice and mod to hyprland

--- options/config/rice.nix
+++ options/config/rice.nix
@@ -1,6 +1,8 @@
 {option-functions, ...}:
 with option-functions;
   option null "rice" (cfg: {
+    border_size = numberOption "border size" 2;
+    rounding = numberOption "rounding" 8;
     gaps = {
       inner = numberOption "gaps between windows (twice as large, since all windows have them)" 5;
       outer = numberOption "gaps between windows and monitor edges" (cfg.gaps.inner * 2);
