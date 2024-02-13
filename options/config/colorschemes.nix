7ab017dbc91b719a6b024ddc36a28b6d1e3b19e7
yunfachi <yunfachi@gmail.com>
2024-02-13 21:46:31 +0300

    feat(colorschemes): add coffee

--- options/config/colorschemes.nix
+++ options/config/colorschemes.nix
@@ -11,7 +11,24 @@ with option-functions; let
 in {
   options.yunfachi = {
     colorschemes = attrsOption "colorschemes" {
-      default = {};
+      coffee = {
+        base00 = "231F1E";
+        base01 = "292423";
+        base02 = "332927";
+        base03 = "5B3F38";
+        base04 = "564642";
+        base05 = "9C8C89";
+        base06 = "F09177";
+        base07 = "42302C";
+        base08 = "DEC36F";
+        base09 = "F24343";
+        base0A = "6E4A40";
+        base0B = "9DCC57";
+        base0C = "DE4040";
+        base0D = "E480AD";
+        base0E = "FEA677";
+        base0F = "6E5E5B";
+      };
     } (submoduleOption colorscheme);
   };
 }
