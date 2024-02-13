3749f2831204b6e5aee6db5bf6feb5742b1dbe53
yunfachi <yunfachi@gmail.com>
2024-02-13 22:08:14 +0300

    feat(rice): add the bar height option

--- options/programs/eww.nix
+++ options/programs/eww.nix
@@ -8,11 +8,4 @@ with option-functions;
     enable = enableOption "eww" true;
 
     config = attrsOption "list of strings, yuck scripts that will be added to config" {} types.str;
-
-    bar = {
-      x = numberOption "x" config.yunfachi.rice.gaps.outer;
-      y = numberOption "y" config.yunfachi.rice.gaps.outer;
-      width = numberOption "width" (1920 - cfg.bar.x * 2); #FIXME: I don't even know how to do this, probably a separate window for each monitor in eww
-      height = numberOption "height" 30;
-    };
   })
