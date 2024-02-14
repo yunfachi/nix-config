a69c219f515768b3d1aedf91de38f0ba300275e9
yunfachi <yunfachi@gmail.com>
2024-02-14 09:37:38 +0300

    feat(config): add fonts

---
+++ modules/config/fonts.nix
@@ -0,0 +1,17 @@
+{
+  module-functions,
+  config,
+  pkgs,
+  ...
+}:
+module-functions.module null "fonts" {
+  fonts = {
+    enableDefaultPackages = true;
+    fontDir.enable = true;
+
+    packages = with pkgs; [
+      font-awesome
+      material-design-icons
+    ];
+  };
+}
