6033f7ece55cad300bd98fef0aca4cb6e4ea050f
yunfachi <yunfachi@gmail.com>
2024-02-14 11:11:16 +0300

    feat(eza): replace tree with eza tree

--- modules/programs/eza.nix
+++ modules/programs/eza.nix
@@ -1,21 +1,27 @@
 {
   module-functions,
   hm,
+  lib,
   ...
 }:
 module-functions.module "programs" "eza" (cfg: {
-  hm.programs.eza = {
-    enable = true;
+  hm.programs = {
+    eza = {
+      enable = true;
 
-    enableAliases = cfg.enableAliases;
-    git = true;
-    icons = cfg.icons;
+      enableAliases = cfg.enableAliases;
+      git = true;
+      icons = cfg.icons;
 
-    extraOptions = [
-      "--hyperlink"
-      "--no-permissions"
-      "--no-user"
-      "--no-time"
-    ];
+      extraOptions = [
+        "--hyperlink"
+        "--no-permissions"
+        "--no-user"
+        "--no-time"
+      ];
+    };
+    bash.shellAliases = lib.mkIf cfg.enableAliases {
+      tree = "eza --tree";
+    };
   };
 })
