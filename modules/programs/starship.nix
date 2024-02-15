439c13f6961a2c16112a13115db135304b7ac6f7
yunfachi <yunfachi@gmail.com>
2024-02-15 17:05:35 +0300

    feat(starship): add directory

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -14,19 +14,25 @@ module-functions.module "programs" "starship" (cfg: {
     enableZshIntegration = false;
 
     settings = {
-      format = "$hostname$nix_shell$character";
+      format = lib.concatStrings ["$hostname" "$nix_shell" "$directory" "$character"];
 
       add_newline = true;
       scan_timeout = 10;
 
       character = {
         success_symbol = "[❯](bold dimmed green)";
-        error_symbol = "[❯](bold red)";
+        error_symbol = "[❯](bold dimmed red)";
+      };
+      directory = {
+        format = "in [$path]($style)[$read_only]($read_only_style) ";
+        style = "bold dimmed green";
+        read_only = "*";
+        read_only_style = "dimmed red";
       };
       hostname = {
         ssh_only = false;
         ssh_symbol = "*";
-        format = "[$ssh_symbol$hostname]($style) ";
+        format = "[$hostname$ssh_symbol]($style) ";
         style = "bold dimmed green";
       };
       nix_shell = {
