334eb3532958992e0ab92a206bd7c215fd821935
yunfachi <yunfachi@gmail.com>
2024-03-28 14:45:06 +0300

    feat(hosts): add shared

--- hosts/dekomori/default.nix
+++ hosts/dekomori/default.nix
@@ -1,8 +1,6 @@
 {user, ...}: {
   host.type = "server";
 
-  user.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJ7BWZyU1dpLM47Y6JI+NKd6sOvXm6JCyqHhrp4NsmD yunfachi@dekomori"];
-
   yunfachi = {
     infras = {
       changelog.enable = true;
---
+++ hosts/mitama/shared.nix
@@ -0,0 +1,3 @@
+{user, ...}: {
+  user.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILG1KltBx01hsQ7/DhRkLGFCADAX7KIumiTkbnE71pi2 yunfachi@mitama"];
+}
