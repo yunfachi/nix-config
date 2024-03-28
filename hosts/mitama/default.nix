67374e5d87512321e17343c4f61820960f5531dd
yunfachi <yunfachi@gmail.com>
2024-03-28 14:33:14 +0300

    feat: add openssh and ssh

--- hosts/mitama/default.nix
+++ hosts/mitama/default.nix
@@ -1,5 +1,8 @@
-{...}: {
+{user, ...}: {
   host.type = "desktop";
+
+  user.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILG1KltBx01hsQ7/DhRkLGFCADAX7KIumiTkbnE71pi2 yunfachi@mitama"];
+
   yunfachi = {
     hardware.displays = [
       {
