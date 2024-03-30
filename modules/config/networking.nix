d52bbfb6892585df2cb10720e8a26a6cfc306d9f
yunfachi <yunfachi@gmail.com>
2024-03-30 08:07:12 +0300

    feat(networking): add nameservers

--- modules/config/networking.nix
+++ modules/config/networking.nix
@@ -1,6 +1,7 @@
 {
   user,
   host,
+  lib,
   ...
 }: {
   networking = {
@@ -8,6 +9,14 @@
 
     firewall.enable = true;
     networkmanager.enable = true;
+    # nameservers = ["1.1.1.1" "1.0.0.1"];
   };
   user.extraGroups = ["networkmanager"];
+
+  environment.etc = {
+    "resolv.conf".text = ''
+      nameserver 1.1.1.1
+      nameserver 1.0.0.1
+    '';
+  };
 }
