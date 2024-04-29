8fbb5167ccfedfecaf3f692c0fd9d5d08e47adf3
yunfachi <yunfachi@gmail.com>
2024-04-29 18:52:33 +0300

    feat(kitty): use a brighter background color

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -14,21 +14,21 @@ module-functions.module "programs" "kitty" {
       tab_bar_edge = "top";
       confirm_os_window_close = 0;
 
-      background = base00.hex;
+      background = base01.hex;
       foreground = base05.hex;
       selection_background = base05.hex;
-      selection_foreground = base00.hex;
+      selection_foreground = base01.hex;
       url_color = base04.hex;
       cursor = base05.hex;
       active_border_color = base03.hex;
       inactive_border_color = base01.hex;
-      active_tab_background = base00.hex;
+      active_tab_background = base01.hex;
       active_tab_foreground = base05.hex;
       inactive_tab_background = base01.hex;
       inactive_tab_foreground = base04.hex;
       tab_bar_background = base01.hex;
 
-      color0 = base00.hex;
+      color0 = base01.hex;
       color1 = base08.hex;
       color2 = base0B.hex;
       color3 = base0A.hex;
