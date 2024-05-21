bc1737a970cc9f911ed3a7e2032b226ff7aafef7
yunfachi <yunfachi@gmail.com>
2024-05-21 21:15:51 +0300

    feat(hosts/ayane): add monitor

--- hosts/ayane/default.nix
+++ hosts/ayane/default.nix
@@ -6,6 +6,14 @@
       amd-gpu.enable = true;
     };
 
+    hardware.displays = [
+      {
+        id = "eDP-1";
+        primary = true;
+        refreshRate = 60;
+      }
+    ];
+
     services = {
       wireguard.privateKeyFile = config.sops.secrets."wireguard/clients/ayane/privateKey".path;
     };
