fabdb69e2a6e08c533d1b749c3c9abfbedcbdf0f
yunfachi <yunfachi@gmail.com>
2024-07-03 11:25:42 +0300

    feat(anime-launcher): rework firefox bookmarks

--- modules/programs/anime-launcher.nix
+++ modules/programs/anime-launcher.nix
@@ -5,26 +5,51 @@
   lib,
   ...
 }:
-module-functions.moduleIfEnabledFinally "programs" "anime-launcher"
+module-functions.moduleIfElseFinally "programs" "anime-launcher"
 {
   nix.settings = inputs.anime-launcher.nixConfig;
   programs.anime-game-launcher.enable = true;
 
   yunfachi.programs.firefox.bookmarks = lib.mkOptionDefault [
     {
-      name = "genshin map";
-      url = "https://act.hoyolab.com/ys/app/interactive-map/index.html?lang=ru-ru#/map/2?shown_types=2,3,154,319,338,17,44,45,46,269,69,508,5,6,194,403,67,71,554,18,471,472,458,327,429,325,326,356,184,466,470,70,64,499,520,77,547,72,73,74,65,155,76,205,258,230,68,231,66,83,259,148,216,75,226,225,270,396,369,398,354,407,397,399,353,79,444,448,459,473,460,445,474,475,464,461,446,447,482,480,481,476,477,478,479,561,551,550,560,559,544,543,552,562,576,578,581,129,359,366,332,321,424,395,330,114,329,223,128,111,224,112,264,127,361,439,130,317,87,253,208,171,85,267,268,147,404,416,415,409,573,574,465,542,189&center=-1217.99,-4411.99&zoom=-2.00";
+      name = "genshin";
+      bookmarks = [
+        {
+          name = "тейват";
+          url = let
+            shown_types = "5,2,3,154,319,338,6,194,403,508,141,325,326,327,429,184,356,458,465,466,470,471,472,554,17,44,269,45,46,8,9,212,411,509,18,70,64,71,77,520,499,547,67,72,73,155,65,74,68,76,205,258,230,259,83,66,216,148,69,75,231,226,225,270,396,399,397,407,369,79,353,354,398,444,445,446,447,448,460,461,464,459,473,474,475,476,480,543,544,550,551,542,479,478,477,481,482,562,576,578,581,561,560,559,552,366,424,395,321,332,330,361,574,575,584,587,573,189,188,187,453,159,156,416,409,147,268,267,415,85,171,87,317,264,112,111,128,439,329,52";
+          in "https://act.hoyolab.com/ys/app/interactive-map/index.html?lang=ru-ru#/map/2?shown_types=${shown_types}&center=-1217.99,-4411.99&zoom=-2.00";
+        }
+        {
+          name = "разлом";
+          url = let
+            shown_types = "453,85,358,359,52,79,231,72,18,70,77,64,46,45,44,17,357,3";
+          in "https://act.hoyolab.com/ys/app/interactive-map/index.html?lang=ru-ru#/map/9?shown_types=${shown_types}&center=-66.00,-233.00&zoom=0.00";
+        }
+        {
+          name = "молитвы";
+          url = "https://paimon.moe/wish";
+        }
+        {
+          name = "отметки";
+          url = "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&lang=ru-ru";
+        }
+        {
+          name = "вики";
+          url = "https://wiki.hoyolab.com/pc/genshin/home";
+        }
+      ];
+    }
+  ];
+}
+{
+  yunfachi.programs.firefox.bookmarks = lib.mkOptionDefault [
+    {
+      name = "genshin daily";
+      url = "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&lang=ru-ru";
     }
   ];
 }
 {
   imports = [inputs.anime-launcher.nixosModules.default];
-
-  yunfachi.programs.firefox.bookmarks = lib.mkOptionDefault [
-    {
-      name = "genshin daily";
-      url = "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&lang=en-us";
-    }
-  ];
-
 }
