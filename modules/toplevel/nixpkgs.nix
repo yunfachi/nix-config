4fce5070f4fb47c7d01734e1f569d32f3995084c
yunfachi <yunfachi@gmail.com>
2024-09-20 13:15:00 +0300

    modules/toplevel/nixpkgs: add allowUnfree to environment variables and nixpkgs/config.nix

--- modules/toplevel/nixpkgs.nix
+++ modules/toplevel/nixpkgs.nix
@@ -2,10 +2,26 @@
   shared.nixpkgs.config = {
     allowUnfree = true;
   };
+  # TODO: /root/.config/nixpkgs/config.nix
+  files."nixpkgs/config.nix".text = ''
+    {
+      allowUnfree = true;
+    }
+  '';
+  variables."NIXPKGS_ALLOW_UNFREE" = 1;
 in
   delib.module {
     name = "nixpkgs";
 
-    nixos.always = shared;
-    home.always = shared;
+    nixos.always =
+      shared
+      // {
+        environment.variables = variables;
+      };
+    home.always =
+      shared
+      // {
+        xdg.configFile = files;
+        home.sessionVariables = variables;
+      };
   }
