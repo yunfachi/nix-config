e47a5b21b14856d60d510735bfd8db22a7af6377
yunfachi <yunfachi@gmail.com>
2024-09-21 19:04:04 +0300

    modules/programs/android-studio: init

---
+++ modules/programs/android-studio.nix
@@ -0,0 +1,22 @@
+{
+  delib,
+  host,
+  user,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "programs.android-studio";
+
+  options = delib.singleEnableOption host.isDesktop;
+
+  nixos.ifEnabled = {
+    services.udev.packages = [pkgs.android-udev-rules];
+    users.groups.adbusers = {};
+    user.extraGroups = ["kvm" "adbusers"];
+  };
+
+  home.ifEnabled = {
+    home.packages = with pkgs; [android-tools android-studio];
+  };
+}
