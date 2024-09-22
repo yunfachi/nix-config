33135a09342963e1dc8d82820405bb14cd1bfdfc
yunfachi <yunfachi@gmail.com>
2024-09-22 12:17:24 +0300

    modules/programs/wireshark: init

---
+++ modules/programs/wireshark.nix
@@ -0,0 +1,27 @@
+{
+  delib,
+  host,
+  user,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "programs.wireshark";
+
+  options = delib.singleEnableOption host.isDesktop;
+
+  nixos.ifEnabled = {
+    users.groups.wireshark = {};
+    user.extraGroups = ["wireshark"];
+
+    security.wrappers.dumpcap = {
+      source = "${pkgs.wireshark}/bin/dumpcap";
+      capabilities = "cap_net_raw,cap_net_admin+eip";
+      owner = "root";
+      group = "wireshark";
+      permissions = "u+rx,g+x";
+    };
+  };
+
+  home.ifEnabled.home.packages = [pkgs.wireshark];
+}
