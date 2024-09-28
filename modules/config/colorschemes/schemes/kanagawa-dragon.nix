7b579dd7acd676dead2f7dd528aed7955d340a1b
yunfachi <yunfachi@gmail.com>
2024-09-28 08:25:03 +0300

    modules/config/colorschemes/schemes/kanagawa-dragon: init

---
+++ modules/config/colorschemes/schemes/kanagawa-dragon.nix
@@ -0,0 +1,33 @@
+{delib, ...}:
+delib.module {
+  name = "coloschemes";
+
+  myconfig.always.colorschemes.kanagawa-dragon = {
+    polarity = "dark";
+
+    base00 = "#181616";
+    base01 = "#1D1C19";
+    base02 = "#282727";
+    base03 = "#393836";
+    base04 = "#9e9b93";
+    base05 = "#a6a69c";
+    base06 = "#c0c3c0";
+    base07 = "#c5c9c5";
+    base08 = "#c4746e";
+    base09 = "#b6927b";
+    base0A = "#c4b28a";
+    base0B = "#87a987";
+    base0C = "#8ea4a2";
+    base0D = "#8ba4b0";
+    base0E = "#a292a3";
+    base0F = "#c4746e";
+    base10 = "#12120f";
+    base11 = "#0d0c0c";
+    base12 = "#c4746e";
+    base13 = "#b98d7b";
+    base14 = "#8a9a7b";
+    base15 = "#8ea4a2";
+    base16 = "#8ba4b0";
+    base17 = "#a292a3";
+  };
+}
