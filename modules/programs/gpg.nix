43f9dd8ddab020be83d73f5b40df2976df1d84d0
yunfachi <yunfachi@gmail.com>
2024-10-06 18:56:09 +0300

    modules/programs/gpg: init

---
+++ modules/programs/gpg.nix
@@ -0,0 +1,26 @@
+{
+  delib,
+  pkgs,
+  homeconfig,
+  ...
+}:
+delib.module {
+  name = "programs.gpg";
+
+  options = delib.singleEnableOption true;
+
+  home.ifEnabled = {myconfig, ...}: {
+    programs.gpg = {
+      enable = true;
+
+      homedir = "${homeconfig.xdg.dataHome}/gnupg";
+    };
+    services.gpg-agent = {
+      enable = true;
+      pinentryPackage =
+        if myconfig.services.gcr.enable
+        then pkgs.pinentry-gnome3
+        else pkgs.pinentry-curves;
+    };
+  };
+}
