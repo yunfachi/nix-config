4e522880136437c9c1a167b31f8e70f1361b370b
yunfachi <yunfachi@gmail.com>
2024-10-20 09:51:07 +0300

    modules/programs/telegram: add persist

--- modules/programs/telegram.nix
+++ modules/programs/telegram.nix
@@ -9,5 +9,7 @@ delib.module {
 
   options = delib.singleEnableOption host.isDesktop;
 
+  myconfig.ifEnabled.persist.user.directories = [".local/share/KotatogramDesktop/tdata"];
+
   home.ifEnabled.home.packages = [pkgs.kotatogram-desktop];
 }
