5dfbf95bfce71d0384011aa0679cbc193a4e2663
yunfachi <yunfachi@gmail.com>
2024-10-20 09:54:35 +0300

    modules/programs/steam: add persist

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -9,5 +9,7 @@ delib.module {
 
   options = delib.singleEnableOption host.isDesktop;
 
+  myconfig.ifEnabled.persist.user.directories = [".steam" ".local/share/Steam"];
+
   home.ifEnabled.home.packages = [pkgs.steam];
 }
