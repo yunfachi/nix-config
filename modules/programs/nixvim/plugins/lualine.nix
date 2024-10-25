6ffcdb0f92000a8e16339127fa38627d115fa889
yunfachi <yunfachi@gmail.com>
2024-10-25 14:52:42 +0300

    modules/programs/nixvim/plugins/lualine: add many settings

--- modules/programs/nixvim/plugins/lualine.nix
+++ modules/programs/nixvim/plugins/lualine.nix
@@ -1,4 +1,8 @@
-{delib, ...}:
+{
+  delib,
+  nixvimLib,
+  ...
+}:
 delib.module {
   name = "programs.nixvim.plugins.lualine";
 
@@ -6,5 +10,57 @@ delib.module {
 
   home.ifEnabled.programs.nixvim.plugins.lualine = {
     enable = true;
+
+    settings = {
+      options = {
+        section_separators = "";
+        component_separators = "|";
+
+        #ignore_focus = ["NvimTree"];
+        disabled_filetypes = ["NvimTree"];
+      };
+
+      sections = {
+        lualine_a = ["mode"];
+        lualine_b = nixvimLib.emptyTable;
+        lualine_c = ["filename"];
+
+        lualine_x = [
+          {
+            __unkeyed-1 = "diagnostics";
+            symbols = {
+              error = "E-";
+              warn = "W-";
+              info = "I-";
+              hint = "H-";
+            };
+          }
+          {
+            __unkeyed-1 = "diff";
+            symbols = {
+              added = "A-";
+              modified = "C-";
+              removed = "R-";
+            };
+          }
+        ];
+        lualine_y = [
+          {
+            __unkeyed-1 = "branch";
+            icons_enabled = false;
+          }
+        ];
+        lualine_z = ["location"];
+      };
+
+      inactive_sections = {
+        lualine_a = nixvimLib.emptyTable;
+        lualine_b = nixvimLib.emptyTable;
+        lualine_c = ["filename"];
+        lualine_x = ["location"];
+        lualine_y = nixvimLib.emptyTable;
+        lualine_z = nixvimLib.emptyTable;
+      };
+    };
   };
 }
