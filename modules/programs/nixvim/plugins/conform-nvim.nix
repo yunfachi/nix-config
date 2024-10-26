f7b1bb32e67fa20df047b010484f6647e188e7ed
yunfachi <yunfachi@gmail.com>
2024-10-26 07:59:09 +0300

    modules/programs/nixvim/plugins/conform-nvim: init

---
+++ modules/programs/nixvim/plugins/conform-nvim.nix
@@ -0,0 +1,23 @@
+{delib, ...}:
+delib.module {
+  name = "programs.nixvim.plugins.conform-nvim";
+
+  options = delib.singleEnableOption true;
+
+  home.ifEnabled.programs.nixvim.plugins.conform-nvim = {
+    enable = true;
+
+    settings = {
+      notify_on_error = true;
+
+      format_on_save = {
+        lsp_format = "fallback";
+        timeout_ms = 500;
+      };
+
+      formatters_by_ft = {
+        nix = ["alejandra"];
+      };
+    };
+  };
+}
