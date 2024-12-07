7abb7c0b7ad786f4c9739687b2068149f31ccb71
yunfachi <yunfachi@gmail.com>
2024-12-07 17:27:40 +0300

    modules/programs/nixvim/plugins/cmp: add mappings

--- modules/programs/nixvim/plugins/cmp.nix
+++ modules/programs/nixvim/plugins/cmp.nix
@@ -8,10 +8,19 @@ delib.module {
     enable = true;
 
     autoEnableSources = true;
-    settings.sources = [
-      {name = "nvim_lsp";}
-      {name = "path";}
-      {name = "buffer";}
-    ];
+    settings = {
+      sources = [
+        {name = "nvim_lsp";}
+        {name = "path";}
+        {name = "buffer";}
+      ];
+
+      mapping = {
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<Esc>" = "cmp.mapping.close()";
+        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+      };
+    };
   };
 }
