ff5590f4d72dd6e1b621001ec3003869000901b8
yunfachi <yunfachi@gmail.com>
2025-01-04 20:42:47 +0300

    modules/programs/schizofox/extensions: add yomitan

--- modules/programs/schizofox/extensions.nix
+++ modules/programs/schizofox/extensions.nix
@@ -10,6 +10,7 @@ delib.module {
       "{0050e3fa-15cc-4fb6-9c73-7354489a810b}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/browser-mopidy-youtube/latest.xpi";
       "{0d48da55-7195-4662-aafc-934d90cb26c2}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/sepia-coffee-v1/latest.xpi";
       "{a138007c-5ff6-4d10-83d9-0afaf0efbe5e}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/history-cleaner/latest.xpi";
+      "{6b733b82-9261-47ee-a595-2dda294a4d08}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/yomitan/latest.xpi";
     };
   };
 }
