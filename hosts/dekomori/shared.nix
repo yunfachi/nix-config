fa096128d1fab6147bf32ddba9dbdfab663f4894
yunfachi <yunfachi@gmail.com>
2025-01-08 14:06:54 +0300

    hosts/dekomori/shared: update ssh key

--- hosts/dekomori/shared.nix
+++ hosts/dekomori/shared.nix
@@ -10,7 +10,7 @@ delib.host {
   shared.myconfig = {name, ...}: {
     services = {
       sshd = {
-        authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJ7BWZyU1dpLM47Y6JI+NKd6sOvXm6JCyqHhrp4NsmD yunfachi@dekomori"];
+        authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHj/D/Zgq7biN7dhAfatnQdVR316NORmyYzvSQwAQIhz yunfachi@dekomori"];
       };
 
       wireguard = {
