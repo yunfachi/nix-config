2658b89b1229a1316ee70425e117748975a896df
yunfachi <yunfachi@gmail.com>
2025-01-10 17:59:03 +0300

    hosts/dekomori/hardware: update

--- hosts/dekomori/hardware.nix
+++ hosts/dekomori/hardware.nix
@@ -8,33 +8,26 @@ delib.host {
   name = "dekomori";
 
   homeManagerSystem = "x86_64-linux";
-  home.home.stateVersion = "24.05";
+  home.home.stateVersion = "24.11";
 
   nixos = {
     nixpkgs.hostPlatform = "x86_64-linux";
-    system.stateVersion = "24.05";
+    system.stateVersion = "24.11";
 
     imports = [(modulesPath + "/profiles/qemu-guest.nix")];
 
     boot = {
-      initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_blk"];
+      initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk"];
       initrd.kernelModules = [];
       kernelModules = ["kvm-amd"];
       extraModulePackages = [];
     };
 
-    fileSystems = {
-      "/" = {
-        device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
-        fsType = "ext4";
-      };
-      "/boot" = {
-        device = "systemd-1";
-        fsType = "autofs";
-      };
+    fileSystems."/" = {
+      device = "/dev/vda2";
+      fsType = "ext4";
     };
 
-    swapDevices = [];
     networking.useDHCP = lib.mkDefault true;
   };
 }
