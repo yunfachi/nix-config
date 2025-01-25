63437c4baabec74941b97c8c1f16ded41e67259c
yunfachi <yunfachi@gmail.com>
2025-01-25 09:18:06 +0300

    modules/toplevel/nix: add trusted-users

--- modules/toplevel/nix.nix
+++ modules/toplevel/nix.nix
@@ -9,6 +9,7 @@
     package = lib.mkForce pkgs.nixVersions.nix_2_18;
     settings = {
       experimental-features = ["nix-command" "flakes"];
+      trusted-users = ["root" "@wheel"];
       warn-dirty = false;
     };
   };
