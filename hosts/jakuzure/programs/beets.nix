9f12b18930164b8b56e111cb52056e3bbedddc0b
yunfachi <yunfachi@gmail.com>
2025-02-12 21:43:14 +0300

    modules/programs/beets: add more options, hosts/jakuzure/programs/beets: init

---
+++ hosts/jakuzure/programs/beets.nix
@@ -0,0 +1,27 @@
+{
+  delib,
+  decryptHostSecret,
+  ...
+}:
+delib.host {
+  name = "jakuzure";
+
+  myconfig = {
+    myconfig,
+    name,
+    ...
+  }: {
+    programs.beets = {
+      enable = true;
+
+      directory = "/media/music";
+      database = "/media/music_beets.db";
+
+      subsonicUpdate = {
+        enable = true;
+        user = decryptHostSecret name "programs/beets/subsonic/user";
+        pass = decryptHostSecret name "programs/beets/subsonic/pass";
+      };
+    };
+  };
+}
--- modules/programs/beets.nix
+++ modules/programs/beets.nix
@@ -1,20 +1,102 @@
 {
   delib,
-  host,
+  lib,
   ...
 }:
 delib.module {
   name = "programs.beets";
 
-  options.programs.beets = with delib; {
-    enable = boolOption host.isDesktop;
-    settings = attrsOption {};
+  options = {myconfig, ...}: {
+    programs.beets = with delib; {
+      enable = boolOption false;
+
+      directory = noDefault (strOption null);
+      database = noDefault (strOption null);
+
+      subsonicUpdate = {
+        enable = boolOption false;
+        # TODO: https://github.com/yunfachi/denix/issues/18
+        url =
+          (
+            if myconfig.services.navidrome.enable
+            then (x: x)
+            else noDefault
+          ) (strOption (
+            if myconfig.services.navidrome.enable
+            then "http://${myconfig.services.navidrome.address}:${toString myconfig.services.navidrome.port}"
+            else null
+          ));
+        user = strOption "admin";
+        pass = strOption "admin";
+        auth = enumOption ["token" "password"] "password";
+      };
+
+      extraSettings = attrsOption {};
+    };
   };
 
   home.ifEnabled = {cfg, ...}: {
     programs.beets = {
       enable = true;
-      inherit (cfg) settings;
+      settings =
+        {
+          inherit (cfg) directory;
+          library = cfg.database;
+
+          /*
+          MAIN
+          */
+          import = {
+            from_scratch = true;
+          };
+          paths = {
+            default = "$albumartist - $album%aunique{}/$track $title";
+            # TODO: set proper values after the first singletons and compilations appear in my library
+            singleton = "Non-Album/$artist/$title";
+            comp = "Compilations/$album%aunique{}/$track $title";
+          };
+
+          /*
+          PLUGINS
+          */
+          subsonic = lib.mkIf cfg.subsonicUpdate.enable {
+            inherit (cfg.subsonicUpdate) url user pass auth;
+          };
+
+          # TODO: configure properly
+          spotify = {
+            show_failures = true;
+          };
+
+          lastgenre = {
+            count = 3; # TODO: count = 1 and good custom whitelist
+          };
+
+          lyrics = {
+            # synced works incorrectly, so the order is important (lrclib is the only one that offers synced lyrics)
+            sources = ["lrclib" "genius" "tekstowo" "google"];
+            synced = true;
+            force = true;
+          };
+
+          plugins =
+            [
+              "spotify"
+              "mbsync"
+
+              "lastgenre"
+              "lyrics"
+
+              "fetchart"
+              "thumbnails"
+              "embedart"
+
+              "duplicates"
+              "missing"
+            ]
+            ++ lib.optional cfg.subsonicUpdate.enable "subsonicupdate";
+        }
+        // cfg.extraSettings;
     };
   };
 }
