aa87c0f0246e54776459af8d01d47f9e763e94c8
yunfachi <yunfachi@gmail.com>
2025-04-13 07:35:32 +0300

    modules/toplevel/dconf: format

--- modules/toplevel/dconf.nix
+++ modules/toplevel/dconf.nix
@@ -12,10 +12,8 @@ delib.module {
     settings = attrsOption {};
   };
 
-  myconfig.always.dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-${colorscheme.polarity}";
-    };
+  myconfig.always.dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-${colorscheme.polarity}";
   };
 
   nixos.ifEnabled.programs.dconf.enable = true;
