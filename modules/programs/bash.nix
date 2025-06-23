02b025a8ac4da30fa314c1d6e4210a3142c9c150
yunfachi <yunfachi@gmail.com>
2025-06-23 09:32:36 +0300

    modules/programs/bash: add history

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -1,12 +1,44 @@
-{delib, ...}:
+{
+  delib,
+  homeconfig,
+  lib,
+  constants,
+  ...
+}:
 delib.module {
   name = "programs.bash";
 
-  options = delib.singleEnableOption true;
+  options.programs.bash = with delib; {
+    enable = boolOption true;
+    historyFile = strOption (homeconfig.home.homeDirectory + "/.bash_history");
+    historySize = intOption 10000;
+  };
 
-  home.ifEnabled.programs.bash = {
-    enable = true;
+  myconfig.ifEnabled = {cfg, ...}: {
+    persist = let
+      cond = lib.hasPrefix homeconfig.home.homeDirectory cfg.historyFile;
+      val = path: {
+        file = path;
+        # FIXME: https://github.com/nix-community/impermanence/issues/273
+        # mode = "u=rw,g=,o=";
+      };
+    in {
+      user.files = lib.mkIf cond [(val (lib.removePrefix homeconfig.home.homeDirectory cfg.historyFile))];
+      files = lib.mkIf (!cond) [(val cfg.historyFile)];
+    };
+  };
 
-    historyControl = ["ignoredups" "ignorespace"];
+  home.ifEnabled = {cfg, ...}: {
+    programs.bash = {
+      enable = true;
+
+      historyControl = ["erasedups" "ignorespace"];
+      inherit (cfg) historyFile historySize;
+
+      initExtra = ''
+        bind '"\e[A": history-search-backward'
+        bind '"\e[B": history-search-forward'
+      '';
+    };
   };
 }
