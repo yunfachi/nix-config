27a1f1fe2e8e4e44d9294ee8adf596adbeda9b9d
yunfachi <yunfachi@gmail.com>
2025-07-09 10:25:12 +0300

    modules/programs/firefox/theme: use base05 instead of base07 for foreground

--- modules/programs/firefox/theme.nix
+++ modules/programs/firefox/theme.nix
@@ -9,7 +9,7 @@ delib.module {
   home.ifEnabled.programs.schizofox.theme.colors = with colorscheme; {
     background-darker = base10.value;
     background = base00.value;
-    foreground = base07.value;
+    foreground = base05.value;
     primary = base02.value;
     border = "00000000";
   };
