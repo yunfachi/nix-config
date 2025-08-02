e0af650e0173d02a90e033290bf239a98658bb5c
yunfachi <yunfachi@gmail.com>
2025-08-02 15:55:14 +0300

    flake: use nixpkgs master, add burpsuitepro

--- flake.nix
+++ flake.nix
@@ -1,8 +1,75 @@
 {
   description = "Modular configuration of NixOS and Home Manager with Denix";
 
+  outputs = {
+    denix,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    mkConfigurations = moduleSystem:
+      denix.lib.configurations rec {
+        inherit moduleSystem;
+        homeManagerUser = "yunfachi";
+
+        paths = [
+          ./hosts
+          ./modules
+          ./rices
+        ];
+
+        extensions = with denix.lib.extensions; [
+          rices
+          args
+          (base.withConfig {
+            args.enable = true;
+
+            hosts.features = {
+              # cli: not must-have (ssh, git, gpg, fail2ban, dnscrypt) utilities like eza, bat, nh, etc.
+              # gui: gui applications and modules that are needed only for gui applications (gnome-keyring, wakatime)
+              features = [
+                "cli"
+                "gui"
+                "gaming"
+                "hacking"
+                "powersave"
+                "wireless"
+              ];
+              defaultByHostType = {
+                desktop = [
+                  "cli"
+                  "gui"
+                  "gaming"
+                  "hacking"
+                ];
+                server = [];
+              };
+            };
+          })
+        ];
+
+        specialArgs = {
+          inherit inputs moduleSystem homeManagerUser;
+        };
+      };
+  in {
+    nixosConfigurations = mkConfigurations "nixos";
+    homeConfigurations = mkConfigurations "home";
+
+    devShells.x86_64-linux = {
+      hacking = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        name = "hacking";
+        packages = with nixpkgs.legacyPackages.x86_64-linux; [
+          # Recon
+          assetfinder
+          nmap
+          python313Packages.dirsearch
+        ];
+      };
+    };
+  };
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -48,7 +115,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      url = "git+https://git.outfoxxed.me/quickshell/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    burpsuitepro = {
+      url = "github:xiv3r/Burpsuite-Professional";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -57,59 +128,5 @@
       url = "github:yunfachi/wallpaper-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-
-  outputs = {
-    denix,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    mkConfigurations = moduleSystem:
-      denix.lib.configurations rec {
-        inherit moduleSystem;
-        homeManagerUser = "yunfachi";
-
-        paths = [
-          ./hosts
-          ./modules
-          ./rices
-        ];
-
-        extensions = with denix.lib.extensions; [
-          args
-          (base.withConfig {
-            args.enable = true;
-
-            hosts.features = {
-              # cli: not must-have (ssh, git, gpg, fail2ban, dnscrypt) utilities like eza, bat, nh, etc.
-              # gui: gui applications and modules that are needed only for gui applications (gnome-keyring, wakatime)
-              features = [
-                "cli"
-                "gui"
-                "gaming"
-                "hacking"
-                "powersave"
-                "wireless"
-              ];
-              defaultByHostType = {
-                desktop = [
-                  "cli"
-                  "gui"
-                  "gaming"
-                  "hacking"
-                ];
-                server = [];
-              };
-            };
-          })
-        ];
-
-        specialArgs = {
-          inherit inputs moduleSystem homeManagerUser;
-        };
-      };
-  in {
-    nixosConfigurations = mkConfigurations "nixos";
-    homeConfigurations = mkConfigurations "home";
   };
 }
