0d88ed2d238928183be5d0a65db9d9cfc6a7267f
yunfachi <yunfachi@gmail.com>
2025-09-21 15:13:32 +0300

    modules/programs/hyprland/rules: add rofi

--- modules/programs/hyprland/rules.nix
+++ modules/programs/hyprland/rules.nix
@@ -6,13 +6,15 @@
 delib.module {
   name = "programs.hyprland";
 
-  home.ifEnabled = {myconfig, ...}: {
-    wayland.windowManager.hyprland.settings = {
-      windowrulev2 =
-        [
+  home.ifEnabled =
+    { myconfig, ... }:
+    {
+      wayland.windowManager.hyprland.settings = {
+        windowrulev2 = [
         ]
         ++ lib.optional myconfig.programs.fcitx5.enable "pseudo, class:fcitx"
+        ++ lib.optional myconfig.programs.rofi.enable "noblur, class:Rofi"
         ++ lib.optional myconfig.programs.nautilus.enable "float, class:org.gnome.NautilusPreviewer";
+      };
     };
-  };
 }
