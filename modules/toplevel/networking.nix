e011d405c5add06bf23550e87a4f62a2d2f0761f
yunfachi <yunfachi@gmail.com>
2025-10-05 21:13:45 +0300

    modules/toplevel/networking: reformat

--- modules/toplevel/networking.nix
+++ modules/toplevel/networking.nix
@@ -7,23 +7,28 @@ delib.module {
   name = "networking";
 
   options.networking = with delib; {
-    nameservers = listOfOption str ["1.1.1.1" "1.0.0.1"];
-    hosts = attrsOfOption (listOf str) {};
+    nameservers = listOfOption str [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    hosts = attrsOfOption (listOf str) { };
   };
 
-  nixos.always = {cfg, ...}: {
-    networking = {
-      hostName = host.name;
+  nixos.always =
+    { cfg, ... }:
+    {
+      networking = {
+        hostName = host.name;
 
-      firewall.enable = true;
-      networkmanager.enable = true;
+        firewall.enable = true;
+        networkmanager.enable = true;
 
-      dhcpcd.extraConfig = "nohook resolv.conf";
-      networkmanager.dns = "none";
+        dhcpcd.extraConfig = "nohook resolv.conf";
+        networkmanager.dns = "none";
 
-      inherit (cfg) hosts nameservers;
+        inherit (cfg) hosts nameservers;
+      };
+
+      user.extraGroups = [ "networkmanager" ];
     };
-
-    user.extraGroups = ["networkmanager"];
-  };
 }
