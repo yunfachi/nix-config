513eb7e38415637b0b5f0897e188819b269b424b
yunfachi <yunfachi@gmail.com>
2025-10-24 11:00:04 +0300

    modules/services/dnscrypt: persist

--- modules/services/dnscrypt.nix
+++ modules/services/dnscrypt.nix
@@ -1,14 +1,28 @@
-{delib, ...}:
+{ delib, ... }:
 delib.module {
   name = "services.dnscrypt";
 
   options = delib.singleEnableOption true;
 
-  myconfig.ifEnabled.networking.nameservers = ["127.0.0.1" "::1"];
+  myconfig.ifEnabled = {
+    persist.directories = [
+      {
+        directory = "/var/lib/private/dnscrypt-proxy";
+        user = "nobody";
+        group = "nogroup";
+        mode = "u=rwx,g=rx,o=rx";
+      }
+    ];
+
+    networking = {
+      nameservers = [
+        "127.0.0.1"
+        "::1"
+      ];
+    };
+  };
 
   nixos.ifEnabled = {
-    networking.resolvconf.useLocalResolver = true;
-
     services.dnscrypt-proxy2 = {
       enable = true;
       settings = {
@@ -20,14 +34,16 @@ delib.module {
             "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
             "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
           ];
-          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          cache_file = "public-resolvers.md";
           minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
         };
       };
     };
 
-    systemd.services.dnscrypt-proxy2.serviceConfig = {
-      StateDirectory = "dnscrypt-proxy";
-    };
+    /*
+      systemd.services.dnscrypt-proxy2.serviceConfig = {
+        StateDirectory = "dnscrypt-proxy";
+      };
+    */
   };
 }
