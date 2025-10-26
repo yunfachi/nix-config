c3ecf240f97fff38806fb25f42a79525d3689479
yunfachi <yunfachi@gmail.com>
2025-10-26 09:48:06 +0300

    hosts/dekomori/shared: add rbw baseUrl

--- hosts/dekomori/shared.nix
+++ hosts/dekomori/shared.nix
@@ -7,52 +7,64 @@
 delib.host {
   name = "dekomori";
 
-  shared.myconfig = {name, ...}: {
-    services = {
-      sshd = {
-        authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHj/D/Zgq7biN7dhAfatnQdVR316NORmyYzvSQwAQIhz yunfachi@dekomori"];
-      };
+  shared.myconfig =
+    { name, ... }:
+    {
+      programs.rbw.baseUrl = decryptSecret "programs/rbw/base_url";
 
-      wireguard = {
-        server = {
-          ip = "127.0.0.1"; # xray dokodemo door
-          port = lib.toInt (decryptSecret "services/wireguard/servers/${name}/port");
-          openFirewall = false;
-
-          hostname = name;
-          interface = "ens3";
-          tunnel = "10.0.1.1";
-          publicKey = decryptSecret "services/wireguard/servers/${name}/publicKey";
+      services = {
+        sshd = {
+          authorizedKeys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHj/D/Zgq7biN7dhAfatnQdVR316NORmyYzvSQwAQIhz yunfachi@dekomori"
+          ];
         };
 
-        clients = {
-          "a52" = {
-            ip = "10.0.1.201";
-            publicKey = decryptSecret "services/wireguard/clients/a52/publicKey";
+        wireguard = {
+          server = {
+            ip = "127.0.0.1"; # xray dokodemo door
+            port = lib.toInt (decryptSecret "services/wireguard/servers/${name}/port");
+            openFirewall = false;
+
+            hostname = name;
+            interface = "ens3";
+            tunnel = "10.0.1.1";
+            publicKey = decryptSecret "services/wireguard/servers/${name}/publicKey";
           };
-          "p11" = {
-            ip = "10.0.1.202";
-            publicKey = decryptSecret "services/wireguard/clients/p11/publicKey";
-          };
-          "m21" = {
-            ip = "10.0.1.203";
-            publicKey = decryptSecret "services/wireguard/clients/m21/publicKey";
+
+          clients = {
+            "a52" = {
+              ip = "10.0.1.201";
+              publicKey = decryptSecret "services/wireguard/clients/a52/publicKey";
+            };
+            "p11" = {
+              ip = "10.0.1.202";
+              publicKey = decryptSecret "services/wireguard/clients/p11/publicKey";
+            };
+            "m21" = {
+              ip = "10.0.1.203";
+              publicKey = decryptSecret "services/wireguard/clients/m21/publicKey";
+            };
+            "w3e" = {
+              ip = "10.0.1.204";
+              publicKey = decryptSecret "services/wireguard/clients/w3e/publicKey";
+            };
           };
         };
-      };
 
-      xray.server = let
-        clients = builtins.fromJSON (decryptSecret "services/xray/clients");
-        mapClients = f: map f (builtins.attrValues clients);
-      in {
-        ip = decryptSecret "hosts/dekomori/ip";
-        port = 443;
+        xray.server =
+          let
+            clients = builtins.fromJSON (decryptSecret "services/xray/clients");
+            mapClients = f: map f (builtins.attrValues clients);
+          in
+          {
+            ip = decryptSecret "hosts/dekomori/ip";
+            port = 443;
 
-        publicKey = decryptSecret "services/xray/server/publicKey";
+            publicKey = decryptSecret "services/xray/server/publicKey";
 
-        clientIds = mapClients (client: client.id);
-        shortIds = mapClients (client: client.shortId);
+            clientIds = mapClients (client: client.id);
+            shortIds = mapClients (client: client.shortId);
+          };
       };
     };
-  };
 }
