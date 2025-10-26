c817b30f345d6689263f7808d9c3f22df57ad1b3
yunfachi <yunfachi@gmail.com>
2025-10-26 09:48:44 +0300

    hosts/dekomori/default: format

--- hosts/dekomori/default.nix
+++ hosts/dekomori/default.nix
@@ -11,53 +11,55 @@ delib.host {
 
   rice = "marukutekurai";
   type = "server";
-  features = ["cli"];
+  features = [ "cli" ];
 
   nixos = {
-    systemd.services.postgresql.after = ["wg-quick-wg0.service"];
+    systemd.services.postgresql.after = [ "wg-quick-wg0.service" ];
 
     boot.loader.grub.device = "/dev/vda";
-    boot.loader.grub.devices = lib.mkForce ["/dev/vda"];
+    boot.loader.grub.devices = lib.mkForce [ "/dev/vda" ];
     zramSwap.enable = true;
   };
 
-  myconfig = {name, ...}: {
-    infras = {
-      changelog.enable = true;
-      deshiro.enable = true;
-    };
-
-    programs = {
-      git.signingKey = "0x3347EA00F28111FC";
-    };
-
-    services = {
-      archisteamfarm.enable = true;
-
-      postgresql = {
-        enable = true;
-
-        openFirewall = true;
-        listen_addresses = [wireguardLocalAddress];
+  myconfig =
+    { name, ... }:
+    {
+      infras = {
+        changelog.enable = true;
+        deshiro.enable = true;
       };
 
-      vaultwarden = {
-        enable = true;
-        database = {
-          backend = "postgresql";
-          password = decryptHostSecret name "services/vaultwarden/postgresql/password";
+      programs = {
+        git.signingKey = "0x3347EA00F28111FC";
+      };
+
+      services = {
+        archisteamfarm.enable = true;
+
+        postgresql = {
+          enable = true;
+
+          openFirewall = true;
+          listen_addresses = [ wireguardLocalAddress ];
+        };
+
+        vaultwarden = {
+          enable = true;
+          database = {
+            backend = "postgresql";
+            password = decryptHostSecret name "services/vaultwarden/postgresql/password";
+          };
+        };
+
+        wireguard = {
+          enable = true;
+          privateKeyFile = decryptHostSecretFile name "services/wireguard/privateKey";
+        };
+
+        xray = {
+          enable = true;
+          server.privateKey = decryptHostSecret name "services/xray/privateKey";
         };
       };
-
-      wireguard = {
-        enable = true;
-        privateKeyFile = decryptHostSecretFile name "services/wireguard/privateKey";
-      };
-
-      xray = {
-        enable = true;
-        server.privateKey = decryptHostSecret name "services/xray/privateKey";
-      };
     };
-  };
 }
