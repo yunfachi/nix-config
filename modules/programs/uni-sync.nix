9afe7a1f74a5db4357f6cba75d20d0ee20689881
yunfachi <yunfachi@gmail.com>
2025-10-27 13:57:43 +0300

    modules/programs/uni-sync: format

--- modules/programs/uni-sync.nix
+++ modules/programs/uni-sync.nix
@@ -1,13 +1,15 @@
-{delib, ...}:
+{ delib, ... }:
 delib.module {
   name = "programs.uni-sync";
 
   options.programs.uni-sync = with delib; {
     enable = boolOption false;
-    devices = listOfOption attrs [];
+    devices = listOfOption attrs [ ];
   };
 
-  nixos.ifEnabled = {cfg, ...}: {
-    hardware.uni-sync = {inherit (cfg) enable devices;};
-  };
+  nixos.ifEnabled =
+    { cfg, ... }:
+    {
+      hardware.uni-sync = { inherit (cfg) enable devices; };
+    };
 }
