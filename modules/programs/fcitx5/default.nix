62b9ceea0f82bdd7459e8f84ef189cd58352a2da
yunfachi <yunfachi@gmail.com>
2025-11-22 14:07:12 +0300

    modules/programs/fcitx5: add env variables

--- modules/programs/fcitx5/default.nix
+++ modules/programs/fcitx5/default.nix
@@ -9,14 +9,22 @@ delib.module {
 
   options = delib.singleEnableOption host.guiFeatured;
 
-  nixos.ifEnabled.i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5 = {
-      waylandFrontend = true;
-      addons = with pkgs; [
-        fcitx5-mozc
-        fcitx5-gtk
-      ];
+  nixos.ifEnabled = {
+    i18n.inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        waylandFrontend = true;
+        addons = with pkgs; [
+          fcitx5-mozc
+          fcitx5-gtk
+        ];
+      };
+    };
+    environment.variables = {
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
     };
   };
 }
